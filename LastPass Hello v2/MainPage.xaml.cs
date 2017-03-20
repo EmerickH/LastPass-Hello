d16404cb56f72ddf339e91cb187deefb866640f1
EmerickH <emerick_herve@outlook.fr>
2017-03-20 17:06:16 +0100

    Introducing Fast Mode

--- LastPass Hello v2/MainPage.xaml.cs
+++ LastPass Hello v2/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,8 +26,11 @@ namespace LastPass_Hello_v2
     public sealed partial class MainPage : Page
     {
         DispatcherTimer timer1;
+        DispatcherTimer timerfast;
         Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
         GoogleAuth auth;
+        Boolean stopfast = false;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -37,14 +41,21 @@ namespace LastPass_Hello_v2
             timer1.Tick += timer1_tick;
 
             timer1.Start();
-            
-            
+
         }
 
         void timer1_tick(object sender, object e)
         {
             if (App.started == true)
             {
+                if (Convert.ToBoolean(localSettings.Values["fast"]) == true && stopfast == false)
+                {
+                    timerfast = new DispatcherTimer();
+                    timerfast.Interval = new TimeSpan(0, 0, 2);
+
+                    timerfast.Tick += timerfast_tick;
+                    timerfast.Start();
+                }
                 Texte.Text = auth.OneTimePassword.ToString();
                 int progress = auth.SecondsToGo;
                 if (progress > progressb.Maximum)
@@ -60,19 +71,35 @@ namespace LastPass_Hello_v2
                     auth.SecretBase32 = localSettings.Values["token"].ToString();
                     auth.Identity = localSettings.Values["id"].ToString();
                     Username.Text = localSettings.Values["id"].ToString();
+                    fastcheck.IsChecked = Convert.ToBoolean(localSettings.Values["fast"]);
                     Texte.Text = auth.OneTimePassword.ToString();
                 }
                 else
                 {
                     localSettings.Values["token"] = "";
                     localSettings.Values["id"] = "";
+                    localSettings.Values["fast"] = false;
                 }
                 App.started = true;
                 ChPara.IsEnabled = true;
+                copyButton.IsEnabled = true;
             }
         }
 
-        private async void savebutton_Click(object sender, RoutedEventArgs e)
+        void timerfast_tick(object sender, object e)
+        {
+            Debug.WriteLine("Start Fast Mode");
+            if (fastcheck.IsChecked == true)
+            {
+                Debug.WriteLine("Fast Mode");
+                var dataPackage = new DataPackage();
+                dataPackage.SetText(auth.OneTimePassword.ToString());
+                Clipboard.SetContent(dataPackage);
+                App.Current.Exit();
+            }
+        }
+
+            private async void savebutton_Click(object sender, RoutedEventArgs e)
         {
             localSettings.Values["token"] = Token.Text;
             localSettings.Values["id"] = Username.Text;
@@ -91,7 +118,6 @@ namespace LastPass_Hello_v2
                 Username.IsEnabled = true;
                 Token.IsEnabled = true;
                 savebutton.IsEnabled = true;
-                copyButton.IsEnabled = true;
                 Token.Text = localSettings.Values["token"].ToString();
             }
             else
@@ -99,7 +125,6 @@ namespace LastPass_Hello_v2
                 Username.IsEnabled = false;
                 Token.IsEnabled = false;
                 savebutton.IsEnabled = false;
-                copyButton.IsEnabled = true;
                 Token.Text = "";
             }
         }
@@ -109,6 +134,19 @@ namespace LastPass_Hello_v2
             var dataPackage = new DataPackage();
             dataPackage.SetText(auth.OneTimePassword.ToString());
             Clipboard.SetContent(dataPackage);
+        }
+
+        private void fastcheck_Click(object sender, RoutedEventArgs e)
+        {
+            localSettings.Values["fast"] = fastcheck.IsChecked;
+        }
+
+        private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.F)
+            {
+                stopfast = true;
+            }
         }
     }
 }
